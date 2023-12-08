46e9e213751fe12c5c585f0e49e4c921e52af085
deucenn <panix.black@web.de>
2023-12-08 16:47:54 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# currency-converter
+
+# Currency Converter
+
+This is a currency converter with the [https://www.exchangerate-api.com/](Exchangegenreate-API).  
+
+
+## API Reference
+
+#### Get all items
+
+```http
+  GET /api/conversion_rates/items
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `api_key` | `string` | **Requires API key** |
+
+
+
+#### add(num1, num2)
+
+Takes two numbers and returns the sum.
+
+
+## Demo
+
+Insert gif or link to demo
+
+
+## Roadmap
+
+- Testing
  
---
+++ script.js
@@ -0,0 +1,59 @@
+const inputEl = document.getElementById("input");
+const incCurEl = document.getElementById("incCur");
+const outCurEl = document.getElementById("outCur");
+const convertEl = document.getElementById("convert");
+const outputEl = document.getElementById("output");
+
+incCurEl.addEventListener("change", function () {
+  console.log("Input: " + incCurEl.value);
+});
+
+outCurEl.addEventListener("change", function () {
+  console.log("Output: " + outCurEl.value);
+});
+
+const currencySymbols = [
+  { code: "USD", symbol: "$" },
+  { code: "EUR", symbol: "€" },
+  { code: "JPY", symbol: "¥" },
+  { code: "GBP", symbol: "£" },
+  { code: "CNY", symbol: "¥" },
+  { code: "AUD", symbol: "$" },
+  { code: "CAD", symbol: "$" },
+  { code: "CHF", symbol: "CHF" },
+  { code: "HKD", symbol: "$" },
+  { code: "SGD", symbol: "$" },
+  { code: "SEK", symbol: "kr" },
+  { code: "KRW", symbol: "₩" },
+  { code: "NOK", symbol: "kr" },
+  { code: "NZD", symbol: "$" },
+  { code: "INR", symbol: "₹" },
+  { code: "MXN", symbol: "$" },
+  { code: "TWD", symbol: "NT$" },
+  { code: "ZAR", symbol: "R" },
+  { code: "BRL", symbol: "R$" },
+  { code: "DKK", symbol: "kr" },
+  { code: "PLN", symbol: "zł" },
+  { code: "THB", symbol: "฿" },
+  { code: "ILS", symbol: "₪" },
+];
+
+console.log(currencySymbols.find(item => item.code === outCurEl.value).symbol)
+
+convertEl.addEventListener("click", () => {
+  fetch(
+    "https://v6.exchangerate-api.com/v6/" +
+      config.apiKey +
+      "/latest/" +
+      incCurEl.value
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const conversionRate = data.conversion_rates[outCurEl.value];
+      const convertedValue = (inputEl.value * conversionRate).toFixed(2);
+      const currMarkup = `<h3>${convertedValue}${currencySymbols.find(item => item.code === outCurEl.value).symbol}</h3>`;
+
+      outputEl.innerHTML = currMarkup; // Clear previous results and display new result
+    })
+    .catch((err) => console.log(err));
+});
